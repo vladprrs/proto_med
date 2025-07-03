c70df575bc96057c19d80aab0df9ae4c7241364d
Vlad Pr <149959639+vladprrs@users.noreply.github.com>
2025-07-03 15:30:55 +0700

    Remove LazyWrapper component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -5,6 +5,3 @@ export { default as SearchField } from './SearchField';
 export { default as Card } from './Card';
 export { default as SegmentedControl } from './SegmentedControl';
 export { default as Toast } from './Toast';
-
-// Компоненты качества и производительности
-export { default as LazyWrapper, withLazyLoading, usePreloadComponent, PreloadOnHover } from './LazyWrapper';
